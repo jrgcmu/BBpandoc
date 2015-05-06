f2025b7854226b6431ef07da156df47a398f4903
John Gardner <jrgcmu@users.noreply.github.com>
2015-05-06 11:02:54 -0500

    Delete pandoc-preview.sh

--- pandoc-preview.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-PATH=$PATH:/usr/local/bin
-
-pandoc -f markdown -t html --number-sections --smart --mathjax="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML" -s
