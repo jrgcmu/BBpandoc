2199a2cff21ecead33230f69d366661789a6c8e5
John Gardner <jrgcmu@users.noreply.github.com>
2015-10-22 14:46:18 -0500

    Update pandoc-pdf.sh

--- pandoc-pdf.sh
+++ pandoc-pdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PATH=$PATH:/usr/texbin:/usr/local/bin
+PATH=$PATH:/usr/local/bin:/Library/TeX/texbin:/usr/texbin
 
 cd "$(dirname "$BB_DOC_PATH")"
-pandoc -o "${BB_DOC_PATH%%.*}".pdf "$BB_DOC_PATH"  --latex-engine=pdflatex  -V geometry:margin=1in
+pandoc -o "${BB_DOC_PATH%%.*}".pdf "$BB_DOC_PATH"  --latex-engine=pdflatex  -V geometry:margin=1in -V fontsize:11pt
 
