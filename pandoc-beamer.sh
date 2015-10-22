07df10a030fac43cb436d93581b0c0e344a769d2
John Gardner <jrgcmu@users.noreply.github.com>
2015-10-22 14:45:06 -0500

    Update pandoc-beamer.sh

--- pandoc-beamer.sh
+++ pandoc-beamer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PATH=$PATH:/usr/texbin:/usr/local/bin
+PATH=$PATH:/usr/local/bin:/Library/TeX/texbin:/usr/texbin
 
 cd "$(dirname "$BB_DOC_PATH")"
 pandoc "$BB_DOC_PATH" -t beamer -o "${BB_DOC_PATH%%.*}".pdf 
